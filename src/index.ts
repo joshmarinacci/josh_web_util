3a9ca254ecddeb56103b6d5a9309151ec02d948e
Josh Marinacci <joshua@marinacci>
2023-08-10 12:19:32 -0700

    added file to json and obj to blob

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,25 @@ export function forceDownloadBlob(title: string, blob: Blob) {
     document.body.removeChild(a)
 }
 
-
 export function canvas_to_blob(canvas:HTMLCanvasElement):Promise<Blob> {
     return new Promise((res,rej)=>{
         canvas.toBlob(blob => {
             res(blob as Blob)
         })
     })
+}
+
+export function file_to_json(file:File):Promise<any> {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader()
+        // @ts-ignore
+        fileReader.onload = event => resolve(JSON.parse(event.target.result))
+        fileReader.onerror = error => reject(error)
+        fileReader.readAsText(file)
+    })
+}
+
+export function obj_to_blob(toJsonObj: any):Promise<Blob> {
+    let str = JSON.stringify(toJsonObj, null, '   ');
+    return Promise.resolve(new Blob([str]));
 }
